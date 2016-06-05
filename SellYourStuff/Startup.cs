999cb7c03e60d8cd065e1424dc87a1f9f663af35
Minu Francis <Minu Francis>
2016-06-05 17:20:11 -0500

    SellYourStuff First COmmit

---
+++ SellYourStuff/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SellYourStuff.Startup))]
+namespace SellYourStuff
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
